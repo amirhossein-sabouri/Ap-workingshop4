b344c15a730895121566395a6990ed202e2ee901
amirhossein-sabouri <sabouriamirhossein9@gmail.com>
2024-04-12 10:45:45 +0330

    getter and setter added

--- Main.java
+++ Main.java
@@ -7,5 +7,21 @@ class Person{
         this.lastname = lastname;
     }
 
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
 
 }
